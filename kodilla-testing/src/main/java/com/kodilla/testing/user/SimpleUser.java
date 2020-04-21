30418a0a60102f5a77249cba0a9bafd2bb901913
damianwenne <damianwenne@gmail.com>
2020-04-21 18:57:12 +0200

    Test1

--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -1,13 +1,18 @@
 package com.kodilla.testing.user;
 
 public class SimpleUser {
-    private String username;
-
-    public SimpleUser(String username) {
+    String username;
+    String realname;
+    public SimpleUser(String username, String realname) {
         this.username = username;
+        this.realname = realname;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getRealname() {
+        return realname;
     }
 }
