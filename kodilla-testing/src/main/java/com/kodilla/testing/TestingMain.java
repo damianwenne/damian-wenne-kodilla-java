e2651f13dd64b79186948666546d7abad6e4c3c0
damianwenne <damianwenne@gmail.com>
2020-04-21 18:57:28 +0200

    Test1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,17 +6,6 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
 
-        // Test SimpleUser
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
         System.out.println("Test - pierwszy test jednostkowy:");
 
         Calculator calculator = new Calculator();
