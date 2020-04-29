98e8f614cc07c6d77c02d18ce49defbb2bb00c0e
damianwenne <damianwenne@gmail.com>
2020-04-29 18:08:52 +0200

    Test problem

---
+++ src/main/java/com/kodilla/testing/collection/App.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        //OddNumbersExterminator one = new OddNumbersExterminator();
+        //one.numbers;
+
+    }
+
+
+}
---
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+class OddNumbersExterminator {
+    ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(11,12,13,14,15));
+
+    public static int exterminate(ArrayList<Integer> numbers) {
+        for (int si: numbers) {
+            ArrayList<Integer> odd = new ArrayList<Integer>();
+            if (si % 2 == 0) {
+                odd.add(si);
+            }
+        }
+        return numbers;
+    }
+}
+
+ */
+
