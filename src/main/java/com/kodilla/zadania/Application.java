8b4332bace5cf0684d326f09191c413333b5dd9d
damianwenne <damianwenne@gmail.com>
2020-04-29 18:23:17 +0200

    Test tworzenia brancha

--- src/main/java/com/kodilla/zadania/Application.java
+++ src/main/java/com/kodilla/zadania/Application.java
@@ -102,5 +102,9 @@ class Application {
         System.out.println("\nHashMap size: " + theMap.size());
         System.out.println("Time taken to remove entry from HashMap: " + (end - begin) + "ns");
 
+        // Test tworzenia nowego brancha
+
+        System.out.println("Damian Wenne");
+
     }
 }
