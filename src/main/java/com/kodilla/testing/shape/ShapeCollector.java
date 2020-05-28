b0ba088df6520c97baf3ccd73a1430d7de4615f1
damianwenne <damianwenne@gmail.com>
2020-05-28 07:43:55 +0200

    Do sprawdzenia

---
+++ src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollector {
+
+    public void addFigure(Shape shape) {
+
+    }
+
+    public void removeFigure(Shape shape) {
+
+    }
+
+    public void getFigure(int n) {
+
+    }
+
+    public void showFigures() {
+
+    }
+}
