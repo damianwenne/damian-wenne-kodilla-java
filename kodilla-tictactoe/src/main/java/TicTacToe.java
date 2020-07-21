d3d049892afe69f101063f9ede20adc818ae59b8
Maciej Rudnicki <maciejru@amazon.com>
2020-07-21 17:30:19 +0200

    MVC and Singleton applied

--- kodilla-tictactoe/src/main/java/TicTacToe.java
+++ kodilla-tictactoe/src/main/java/TicTacToe.java
@@ -1,7 +1,4 @@
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,7 +11,6 @@ import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
 
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,94 +18,41 @@ public class TicTacToe extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-
         // Top
         Label topText = new Label("Tic Tac Toe");
         topText.setFont(new Font("Calibri", 60));
 
         //Center
-        Button field11 = new Button();
-        Button field12 = new Button();
-        Button field13 = new Button();
-        Button field21 = new Button();
-        Button field22 = new Button();
-        Button field23 = new Button();
-        Button field31 = new Button();
-        Button field32 = new Button();
-        Button field33 = new Button();
-        field11.setPrefSize(150,150);
-        field12.setPrefSize(150,150);
-        field13.setPrefSize(150,150);
-        field21.setPrefSize(150,150);
-        field22.setPrefSize(150,150);
-        field23.setPrefSize(150,150);
-        field31.setPrefSize(150,150);
-        field32.setPrefSize(150,150);
-        field33.setPrefSize(150,150);
+        Controller controller = Controller.INSTANCE;
+        FieldButton field11 = new FieldButton(1,1);
+        controller.addButton(field11);
+        FieldButton field12 = new FieldButton(1,2);
+        controller.addButton(field12);
+        FieldButton field13 = new FieldButton(1,3);
+        controller.addButton(field13);
+        FieldButton field21 = new FieldButton(2,1);
+        controller.addButton(field21);
+        FieldButton field22 = new FieldButton(2,2);
+        controller.addButton(field22);
+        FieldButton field23 = new FieldButton(2,3);
+        controller.addButton(field23);
+        FieldButton field31 = new FieldButton(3,1);
+        controller.addButton(field31);
+        FieldButton field32 = new FieldButton(3,2);
+        controller.addButton(field32);
+        FieldButton field33 = new FieldButton(3,3);
+        controller.addButton(field33);
+
         GridPane gridPane = new GridPane();
-        gridPane.add(field11,0,0);
-        gridPane.add(field12,1,0);
-        gridPane.add(field13,2,0);
-        gridPane.add(field21,0,1);
-        gridPane.add(field22,1,1);
-        gridPane.add(field23,2,1);
-        gridPane.add(field31,0,2);
-        gridPane.add(field32,1,2);
-        gridPane.add(field33,2,2);
-        field11.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field11.setText("X");
-            }
-        });
-        field12.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field12.setText("X");
-            }
-        });
-        field13.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field13.setText("X");
-            }
-        });
-        field21.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field21.setText("X");
-            }
-        });
-        field22.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field22.setText("X");
-            }
-        });
-        field23.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field23.setText("X");
-            }
-        });
-        field31.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field31.setText("X");
-            }
-        });
-        field32.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field32.setText("X");
-            }
-        });
-        field33.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                field33.setText("X");
-            }
-        });
+        gridPane.add(field11, 0, 0);
+        gridPane.add(field12, 1, 0);
+        gridPane.add(field13, 2, 0);
+        gridPane.add(field21, 0, 1);
+        gridPane.add(field22, 1, 1);
+        gridPane.add(field23, 2, 1);
+        gridPane.add(field31, 0, 2);
+        gridPane.add(field32, 1, 2);
+        gridPane.add(field33, 2, 2);
 
         // Left
         Button saveButton = new Button("Save");
@@ -117,12 +60,12 @@ public class TicTacToe extends Application {
         Button restartButton = new Button("Restart");
         restartButton.setMinWidth(55);
         Button oButton = new Button("O");
-        oButton.setMinSize(55,55);
+        oButton.setMinSize(55, 55);
         Button xButton = new Button("X");
-        xButton.setMinSize(55,55);
+        xButton.setMinSize(55, 55);
         VBox leftMenu = new VBox();
-        leftMenu.getChildren().addAll(oButton, xButton,saveButton,restartButton);
-        leftMenu.setPadding(new Insets(0,5,5,5));
+        leftMenu.getChildren().addAll(oButton, xButton, saveButton, restartButton);
+        leftMenu.setPadding(new Insets(0, 5, 5, 5));
         leftMenu.setSpacing(5);
 
         BorderPane borderPane = new BorderPane();
