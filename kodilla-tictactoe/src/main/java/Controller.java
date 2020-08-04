605567f1aac94a2eeb00a354462f4464f396bf70
damianwenne <damianwenne@gmail.com>
2020-08-04 20:21:05 +0200

    Added PopUp and delay

--- kodilla-tictactoe/src/main/java/Controller.java
+++ kodilla-tictactoe/src/main/java/Controller.java
@@ -1,6 +1,17 @@
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Controller {
 
@@ -8,61 +19,40 @@ public class Controller {
     private static final Random RANDOM = new Random();
 
     private final List<FieldButton> fieldButtons = new ArrayList<>();
+    private Label theWinnerLabel = new Label();
 
     private Controller() {
     }
-
 
     void addButton(FieldButton fieldButton) {
         fieldButtons.add(fieldButton);
     }
 
-    boolean pplayerCheck(int a, int b, int c) {
-        return fieldButtons.get(a).getText().equals("X") && fieldButtons.get(b).getText().equals("X") && fieldButtons.get(c).getText().equals("X");
-    }
-
-    void playerCheck() {
-        if (pplayerCheck(0, 1, 2)) {
-            System.out.println("Wygrałeś");
-        } else if (pplayerCheck(3, 4, 5)) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(6).getText().equals("X") && fieldButtons.get(7).getText().equals("X") && fieldButtons.get(8).getText().equals("X")) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(0).getText().equals("X") && fieldButtons.get(3).getText().equals("X") && fieldButtons.get(6).getText().equals("X")) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(1).getText().equals("X") && fieldButtons.get(4).getText().equals("X") && fieldButtons.get(7).getText().equals("X")) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(2).getText().equals("X") && fieldButtons.get(5).getText().equals("X") && fieldButtons.get(8).getText().equals("X")) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(0).getText().equals("X") && fieldButtons.get(4).getText().equals("X") && fieldButtons.get(8).getText().equals("X")) {
-            System.out.println("Wygrałeś");
-        } else if (fieldButtons.get(2).getText().equals("X") && fieldButtons.get(4).getText().equals("X") && fieldButtons.get(6).getText().equals("X")) {
-            System.out.println("Wygrałeś");
+    boolean check(String sign) {
+        if (checkSingle(0, 1, 2, sign)) {
+            return true;
+        } else if (checkSingle(3, 4, 5, sign)) {
+            return true;
+        } else if (checkSingle(6, 7, 8, sign)) {
+            return true;
+        } else if (checkSingle(0, 3, 6, sign)) {
+            return true;
+        } else if (checkSingle(1, 4, 7, sign)) {
+            return true;
+        } else if (checkSingle(2, 5, 8, sign)) {
+            return true;
+        } else if (checkSingle(0, 4, 8, sign)) {
+            return true;
+        } else if (checkSingle(2, 4, 6, sign)) {
+            return true;
         }
+        return false;
     }
 
-    boolean ccompCheck(int d, int e, int f) {
-        return fieldButtons.get(d).getText().equals("O") && fieldButtons.get(e).getText().equals("O") && fieldButtons.get(f).getText().equals("O");
-    }
-
-    void compCheck() {
-        if (ccompCheck(0,1,2)) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(3).getText().equals("O") && fieldButtons.get(4).getText().equals("O") && fieldButtons.get(5).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(6).getText().equals("O") && fieldButtons.get(7).getText().equals("O") && fieldButtons.get(8).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(0).getText().equals("O") && fieldButtons.get(3).getText().equals("O") && fieldButtons.get(6).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(1).getText().equals("O") && fieldButtons.get(4).getText().equals("O") && fieldButtons.get(7).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(2).getText().equals("O") && fieldButtons.get(5).getText().equals("O") && fieldButtons.get(8).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(0).getText().equals("O") && fieldButtons.get(4).getText().equals("O") && fieldButtons.get(8).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        } else if (fieldButtons.get(2).getText().equals("O") && fieldButtons.get(4).getText().equals("O") && fieldButtons.get(6).getText().equals("O")) {
-            System.out.println("Przegrałeś");
-        }
+    boolean checkSingle(int a, int b, int c, String sign) {
+        return fieldButtons.get(a).getText().equals(sign)
+                && fieldButtons.get(b).getText().equals(sign)
+                && fieldButtons.get(c).getText().equals(sign);
     }
 
     void restart() {
@@ -71,6 +61,19 @@ public class Controller {
         }
     }
 
+    void compMove() {
+        FieldButton compButton = fieldButtons.get(RANDOM.nextInt(9));
+        try {
+            Thread.sleep(1*1000);
+            if (compButton.getText().length() < 1) {
+                compButton.setText("O");
+            } else {
+                compMove();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void click(FieldButton button) {
         if (button.getText().length() > 0) {
@@ -81,16 +84,47 @@ public class Controller {
                 button.getCol(),
                 button.getRow()));
         button.setText("X");
-        playerCheck();
-
-        // Computer move, To Do: draw only free button
-
-        FieldButton compButton = fieldButtons.get(RANDOM.nextInt(9));
-        while (compButton.getText().length() < 1) {
-            compButton.setText("O");
-            compCheck();
+        if (check("X")) {
+            winnerPopUp();
+            theWinnerLabel.setText("You Won!");
+            System.out.println("Won");
         }
 
+        try {
+            compMove();
+        } catch (StackOverflowError e) {
+            System.out.println("Game Over");
+        }
+
+        if (check("O")) {
+            winnerPopUp();
+            theWinnerLabel.setText("Computer Won!");
+            System.out.println("Lost");
+        }
     }
 
+    public void winnerPopUp() {
+        Stage winnerPopUpWindow = new Stage();
+
+        winnerPopUpWindow.initModality(Modality.APPLICATION_MODAL);
+        winnerPopUpWindow.setMinWidth(250);
+        winnerPopUpWindow.setMinHeight(200);
+        winnerPopUpWindow.setTitle("Game Over!");
+        Button okToCloseButton = new Button("OK");
+        okToCloseButton.addEventHandler(ActionEvent.ACTION, (e) -> {
+            winnerPopUpWindow.close();
+            restart();
+        });
+
+        theWinnerLabel.setFont(new Font(30));
+        VBox winnerPopUpVBoxLayout = new VBox(10);
+        winnerPopUpVBoxLayout.getChildren().addAll(theWinnerLabel, okToCloseButton);
+        winnerPopUpVBoxLayout.setAlignment(Pos.CENTER);
+
+        Scene winnerPopUpScene = new Scene(winnerPopUpVBoxLayout);
+        winnerPopUpWindow.setScene(winnerPopUpScene);
+        winnerPopUpWindow.setOnCloseRequest(e -> restart());
+        winnerPopUpWindow.show();
+    }
 }
+
